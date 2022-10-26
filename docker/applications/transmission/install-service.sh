28153c3f51a858465a345fdcd53d99a49242a9f7
kvaga <kvagalex@yandex.ru>
2022-10-27 00:21:44 +0300

    Fixed service file of transmission

--- docker/applications/transmission/install-service.sh
+++ docker/applications/transmission/install-service.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-sudo cp docker.transmission.service /etc/systemd/system
 sudo systemctl stop docker.transmission.service
+sudo systemctl disable docker.transmission.service
+sudo cp docker.transmission.service /etc/systemd/system
 sudo systemctl daemon-reload
-sudo systemctl enable docker.transmission.service
 sudo systemctl start docker.transmission.service
+sudo systemctl enable docker.transmission.service
 sudo systemctl status docker.transmission.service
